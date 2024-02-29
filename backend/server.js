a2c2406df4eeed374b60b0b13f7f90b46bab4aea
MircoSut <mircosut76@gmail.com>
2024-02-29 11:19:48 +0100

    Added signup functionalities +

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,18 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  // root route http://localhost:5000/
-  res.send("Hello Word!");
-});
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming request with json payloads (from req.body)
 
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   // root route http://localhost:5000/
+//   res.send("Hello Word!");
+// });
 
 app.listen(PORT, () => {
   connectToMongoDB();
